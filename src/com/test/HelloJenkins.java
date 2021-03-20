7764ec1cd4fe925c9ed5de75145e93679ba610d7
unknown <santiagoisaza0@gmail.com>
2021-03-20 13:18:45 -0500

    initial commit

---
+++ src/com/test/HelloJenkins.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class HelloJenkins {
+    public static void main(String[] args) {
+            for(int i=0;i<10;i++){
+                System.out.println( "Hola jenkins" +i);
+            }
+    }
+}
